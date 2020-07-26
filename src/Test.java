63bd681a40e061082877eaf3f2a3f92f7d86364b
放牛的coder <937166934@qq.com>
2020-07-26 10:47:48 +0800

    hello you

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,11 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("123");
+        long freeMemory = Runtime.getRuntime().freeMemory();
+        System.out.println("freeMemory= "+freeMemory);
+        int processors = Runtime.getRuntime().availableProcessors();
+        System.out.println("processors= "+processors);
+        long totalMemory = Runtime.getRuntime().totalMemory();
+        System.out.println("totalMemory= "+totalMemory);
     }
 }
